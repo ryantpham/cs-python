762ddf625490837fb49ff267146cdaabcff846e3
Ryan Pham <rtpham10@gmail.com>
2024-04-17 20:45:26 -0700

    initial commit

---
+++ ps1/ps1a.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 17 20:41:29 2024
+
+@author: ryanpham
+"""
+
+print("PA 1")
---
+++ ps1/ps1b.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 17 20:42:11 2024
+
+@author: ryanpham
+"""
+
+print("PA 2")
