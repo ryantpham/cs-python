02871613ff3493e613f0c9c9ea9f9b5560961fc5
Ryan Pham <rtpham10@gmail.com>
2024-04-17 20:06:35 -0700

    spyder ide test

---
+++ ps0/pyTest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 17 19:52:47 2024
+
+@author: ryanpham
+"""
+
+import math
+print(math.sqrt(16))
+print(math.cos(0))
+
+a = 6+4*10
+#expect 46
+b = (6+4)*10
+#expect 100
+print(a)
+print(b)
+
